855125e21214ea39221375f0c5e3401eb59148ae
shb0702 <shb9036@naver.com>
2023-05-24 23:47:59 +0900

    test commit

---
+++ src/main/java/Controller/devCommunityController.java
@@ -0,0 +1,16 @@
+package Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class devCommunityController {
+
+	
+
+	public String devCommunityList() {
+		
+		
+		
+		return null;
+	}
+}
